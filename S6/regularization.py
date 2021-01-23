a69911e2c04f30e61fd565dd4d4f8071943ff1f4
rampfire <abishek.ur@gmail.com>
2021-01-23 21:35:57 +0530

    added all the files for S7

--- S6/regularization.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 23 19:24:15 2021
-
-@author: rampfire
-"""
-
-
--- S6/training.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 23 19:23:44 2021
-
-@author: rampfire
-"""
-
-

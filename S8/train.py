99198457eead2d76376742f6c5ca261b0f5c558f
rampfire <abishek.ur@gmail.com>
2021-01-31 18:48:07 +0530

    added progress bar

--- S8/train.py
+++ S8/train.py
@@ -5,7 +5,7 @@ Created on Sun Jan 31 11:43:16 2021
 
 @author: rampfire
 """
-
+from tqdm import tqdm
 
 def train(epoch,net,device,trainloader,optimizer,criterion):
     print('\nEpoch: %d' % epoch)
@@ -13,19 +13,20 @@ def train(epoch,net,device,trainloader,optimizer,criterion):
     train_loss = 0
     correct = 0
     total = 0
-    for batch_idx, (inputs, targets) in enumerate(trainloader):
-        inputs, targets = inputs.to(device), targets.to(device)
-        optimizer.zero_grad()
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
-
-        train_loss += loss.item()
-        _, predicted = outputs.max(1)
-        total += targets.size(0)
-        correct += predicted.eq(targets).sum().item()
-
-        print(f'batch_idx:{batch_idx},loss:{(train_loss/(batch_idx+1))},acc:{100.*correct/total}')
+    with tqdm(total=len(trainloader)) as pbar:
+        for batch_idx, (inputs, targets) in enumerate(trainloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
+    
+            train_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+            pbar.update(1)
+        # print(f'batch_idx:{batch_idx},loss:{(train_loss/(batch_idx+1))},acc:{100.*correct/total}')
         # progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
         #              % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))

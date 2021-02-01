afdda4edb18ce3ec27b4828dea5e53aae1869bc5
rampfire <abishek.ur@gmail.com>
2021-02-01 14:16:49 +0530

    session 9 files added

---
+++ S9/test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 31 11:49:34 2021
+
+@author: rampfire
+"""
+import torch
+from tqdm import tqdm
+
+def test(epoch,net,testloader,device,criterion):
+    global best_acc
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        with tqdm(total=len(testloader)) as pbar:
+            for batch_idx, (inputs, targets) in enumerate(testloader):
+                inputs, targets = inputs.to(device), targets.to(device)
+                outputs = net(inputs)
+                loss = criterion(outputs, targets)
+    
+                test_loss += loss.item()
+                _, predicted = outputs.max(1)
+                total += targets.size(0)
+                correct += predicted.eq(targets).sum().item()
+                # print(f'batch_idx:{batch_idx},loss:{(test_loss/(batch_idx+1))},acc:{100.*correct/total}')
+                pbar.update(1)
+                # progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                #              % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+    return (test_loss/(batch_idx+1)),100.*correct/total
---
+++ S9/train.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 31 11:43:16 2021
+
+@author: rampfire
+"""
+from tqdm import tqdm
+
+def train(epoch,net,device,trainloader,optimizer,criterion):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    with tqdm(total=len(trainloader)) as pbar:
+        for batch_idx, (inputs, targets) in enumerate(trainloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
+    
+            train_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+            pbar.update(1)
+    return (train_loss/(batch_idx+1)),100.*correct/total
+        # print(f'batch_idx:{batch_idx},loss:{(train_loss/(batch_idx+1))},acc:{100.*correct/total}')
+        # progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+        #              % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
